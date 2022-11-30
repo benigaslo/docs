7c6a1e6b18fe095716d5287ba9d048f5d268d93e
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2022-11-30 16:45:01 +0100

    Update ubuntu2004-aules-eso-config.sh

--- ubuntu2004-aules-eso-config.sh
+++ ubuntu2004-aules-eso-config.sh
@@ -1,16 +1,22 @@
 apt update
-apt install curl openjdk-17-jre-headless openssh-server
-apt remove gnome-initial-setup apt remove update-notifier
+apt install curl openjdk-8-jre-headless openssh-server
+apt remove gnome-initial-setup update-notifier
 
 # canviar aplicacions favorites
 printf 'user-db:user\nsystem-db:benigaslo' > /etc/dconf/profile/user
 mkdir -p /etc/dconf/db/benigaslo.d/ && printf "[org/gnome/shell]\nfavorite-apps = ['firefox.desktop', 'nautilus.desktop']" > /etc/dconf/db/benigaslo.d/00_benigaslo_settings
-dconf update; chmod a+rx -R /etc/dconf
 
-# instalar clickcontrol
-wget https://github.com/benigaslo/clickcontrol/releases/download/clickcontrol/Instalador-ClickControlDS-Ubuntu-x64 && chmod +x Instalador-ClickControlDS-Ubuntu-x64 && sudo ./Instalador-ClickControlDS-Ubuntu-x64
+# amagar la llista de usuaris a la pantalla de login
+printf 'user-db:user\nsystem-db:gdm\nfile-db:/usr/share/gdm/greeter-dconf-defaults' > /etc/dconf/profile/gdm
+mkdir -p /etc/dconf/db/gdm.d/ && printf '[org/gnome/login-screen]\ndisable-user-list=true' > /etc/dconf/db/gdm.d/00-login-screen
+dconf update; chmod a+rx -R /etc/dconf
 
 # configurar ssh
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
 mkdir -p /root/.ssh && sh -c 'curl -fsSL  https://github.com/benigaslo/docs/releases/download/id_rsa/id_rsa.pub >> /root/.ssh/authorized_keys'
+
+# instalar clickcontrol
+wget https://github.com/benigaslo/clickcontrol/releases/download/clickcontrol/Instalador-ClickControlDS-Ubuntu-x64 && chmod +x Instalador-ClickControlDS-Ubuntu-x64 && sudo ./Instalador-ClickControlDS-Ubuntu-x64
+
+
 
