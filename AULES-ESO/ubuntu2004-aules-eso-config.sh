c913694d9723db21285a31972909d935254bd8eb
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2023-09-28 11:53:15 +0200

    Update ubuntu2004-aules-eso-config.sh

--- AULES-ESO/ubuntu2004-aules-eso-config.sh
+++ AULES-ESO/ubuntu2004-aules-eso-config.sh
@@ -1,22 +1,58 @@
 apt update
-apt install -y curl openjdk-8-jre-headless openssh-server
+apt install -y curl openssh-server
 apt remove gnome-initial-setup update-notifier
 
+#
 # canviar aplicacions favorites
-printf 'user-db:user\nsystem-db:benigaslo' > /etc/dconf/profile/user
-mkdir -p /etc/dconf/db/benigaslo.d/ && printf "[org/gnome/shell]\nfavorite-apps = ['firefox.desktop', 'nautilus.desktop']" > /etc/dconf/db/benigaslo.d/00_benigaslo_settings
+# deshabilitar canviar d'usuari
+# deshabilitar bloqueig de pantalla
+#
+cat << EOF > /etc/dconf/profile/user
+user-db:user\nsystem-db:benigaslo'
+EOF
 
+mkdir -p /etc/dconf/db/benigaslo.d/ 
+
+cat << EOF > /etc/dconf/db/benigaslo.d/00_benigaslo_settings
+[org/gnome/shell]
+favorite-apps = ['firefox.desktop', 'nautilus.desktop'] 
+
+[org/gnome/desktop/lockdown]
+disable-user-switching=true
+disable-lock-screen=true
+EOF
+
+#
 # amagar la llista de usuaris a la pantalla de login
-printf 'user-db:user\nsystem-db:gdm\nfile-db:/usr/share/gdm/greeter-dconf-defaults' > /etc/dconf/profile/gdm
-mkdir -p /etc/dconf/db/gdm.d/ && printf '[org/gnome/login-screen]\ndisable-user-list=true' > /etc/dconf/db/gdm.d/00-login-screen
-dconf update; chmod a+rx -R /etc/dconf
+#
+cat << EOF > /etc/dconf/profile/gdm
+user-db:user
+system-db:gdm
+file-db:/usr/share/gdm/greeter-dconf-defaults
+EOF
 
+mkdir -p /etc/dconf/db/gdm.d/ 
+
+cat << EOF > /etc/dconf/db/gdm.d/00-login-screen
+[org/gnome/login-screen]
+disable-user-list=true
+EOF
+
+dconf update 
+chmod a+rx -R /etc/dconf
+
+#
+# Deshabilitar la hibernacio
+#
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+#
 # configurar ssh
+#
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
-mkdir -p /root/.ssh && sh -c 'curl -fsSL  https://github.com/benigaslo/docs/releases/download/id_rsa/id_rsa.pub > /root/.ssh/authorized_keys'
+mkdir -p /root/.ssh 
+sh -c 'curl -fsSL  https://github.com/benigaslo/docs/releases/download/id_rsa/id_rsa.pub > /root/.ssh/authorized_keys'
 
 # instalar clickcontrol
-wget https://github.com/benigaslo/clickcontrol/releases/download/clickcontrol/Instalador-ClickControlDS-Ubuntu-x64 && chmod +x Instalador-ClickControlDS-Ubuntu-x64 && sudo ./Instalador-ClickControlDS-Ubuntu-x64
-
-
+# wget https://github.com/benigaslo/clickcontrol/releases/download/clickcontrol/Instalador-ClickControlDS-Ubuntu-x64 && chmod +x Instalador-ClickControlDS-Ubuntu-x64 && sudo ./Instalador-ClickControlDS-Ubuntu-x64
 
