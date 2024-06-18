1a259b58a8c35d428801558c18a430f890356489
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-06-18 12:49:54 +0200

    Update u24-base-config.sh

--- AULES-INFO/u24-base-config.sh
+++ AULES-INFO/u24-base-config.sh
@@ -16,7 +16,7 @@ sudo add-apt-repository ppa:gns3/ppa
 
 apt update
 
-apt install -y openssh-server nmap tree plocate default-jre wireshark ubuntu-restricted-extras vlc chromium-browser gimp inkscape git sssd fusioninventory-agent nodejs gns3-gui bridge-utils mono-complete
+apt install -y openssh-server nmap tree plocate default-jre wireshark ubuntu-restricted-extras vlc chromium-browser gimp inkscape dia git sssd fusioninventory-agent nodejs gns3-gui bridge-utils mono-complete
 
 curl -fsSL https://get.docker.com | bash -
 
