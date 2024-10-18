c8fd62d2ad8098a342bf690f4c2aeec79267d2d5
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2024-10-18 09:57:17 +0200

    Update ubuntu2204-aules-eso-config.sh

--- AULES-ESO/ubuntu2204-aules-eso-config.sh
+++ AULES-ESO/ubuntu2204-aules-eso-config.sh
@@ -12,6 +12,13 @@ snap remove thunderbird
 snap remove rhythmbox
 snap refresh
 
+
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install brave-browser
+
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
