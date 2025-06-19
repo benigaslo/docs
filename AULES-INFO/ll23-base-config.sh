9ac73b1a0988cf9aa830bfbf9b3025197ec99a8b
gfalcoperez <114737321+gfalcoperez@users.noreply.github.com>
2025-06-19 12:44:03 +0200

    Update ll23-base-config.sh

--- AULES-INFO/ll23-base-config.sh
+++ AULES-INFO/ll23-base-config.sh
@@ -1,41 +1,19 @@
 # ====================================================================
 # Instalar Varios
 # ====================================================================
-
-apt remove xournalpp xaos tuxtype scribus ri-li qstopmotion posterazor onboard musescore3 marble librecad kig gperiodic avogadro intef-exe-appimage gdis hotpotatoes
-
-# desinstalar transmission skanlite libgphoto2-6 laby kget kamoso k3b jclic imagination filezilla gparted tuxmath
-
-# groovy-console gestor-particions-kde
-
-
-# kde-sms
-
-apt update
-apt install -y curl
-
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-sudo add-apt-repository -y ppa:gns3/ppa
-
-apt update
-
-apt install -y openssh-server pssh nmap tree plocate default-jre wireshark ubuntu-restricted-extras vlc chromium-browser gimp inkscape dia git nodejs gns3-gui bridge-utils virt-manager
-
-curl -fsSL https://get.docker.com | bash -
-
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
-
 snap install code --classic
-snap install kdenlive
-snap install audacity
+snap install intellij-idea-community --classic
+snap install thonny
 
-snap remove thunderbird
 
-snap refresh
+# apt remove xournalpp xaos tuxtype scribus ri-li qstopmotion posterazor onboard musescore3 marble librecad kig gperiodic avogadro intef-exe-appimage gdis hotpotatoes
+# apt autoremove
+# desinstalar transmission skanlite libgphoto2-6 laby kget kamoso k3b jclic imagination filezilla gparted tuxmath
+# groovy-console gestor-particions-kde
+# kde-sms thunderbird
 
-apt remove rhythmbox deja-dup
+
+
 
 # ==============
 # Flickering bug
@@ -58,18 +36,16 @@ useradd -m -p "\$y\$j9T\$gZ.2jbvd92uRZ6RYUMl5p0\$zv4D/6oKY2EqErtTrSjOcNQZ2lQuP7Y
 # configurar ssh
 # ====================================================================
 
-# echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
 echo 'PasswordAuthentication no' > /etc/ssh/sshd_config.d/PasswordAuthentication.conf
 mkdir -p /root/.ssh 
 curl -fsSLo /root/.ssh/authorized_keys https://github.com/benigaslo/docs/releases/download/id_rsa/id_rsa.pub
-# sh -c 'curl -fsSL  https://github.com/benigaslo/docs/releases/download/id_rsa/id_rsa.pub > /root/.ssh/authorized_keys'
 
 
 # ====================================================================
 # Afegir a tots els usuaris (inclosos iddigital) als grups necesaris
 # ====================================================================
 
-# magicament el meu usuari g.falcoperez esta afegit al grup docker
+# 'magicament' el meu usuari g.falcoperez esta afegit al grup docker
 #cat << EOF > /etc/security/group.conf
 #*;*;*;Al;ubridge,libvirt,kvm,wireshark,docker,sudo,adm
 #EOF
@@ -79,7 +55,8 @@ curl -fsSLo /root/.ssh/authorized_keys https://github.com/benigaslo/docs/release
 # Docker
 # ====================================================================
 
-# curl -fsSL https://get.docker.com | bash -
+# [x]  curl -fsSL https://get.docker.com | bash -
+# Instalar via ZeroCenter
 
 # Configurar la red default docker
 mkdir -p /etc/docker/
